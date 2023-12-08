219e075522dc3b2f0fa7942dae2df24ca4018331
lucas <124883077+lucasszx@users.noreply.github.com>
2023-12-08 09:53:36 -0300

    Melhoria na class Agendamento

--- Projeto integrador Lucas Nunes Ferreira/src/model/Agendamento.java
+++ Projeto integrador Lucas Nunes Ferreira/src/model/Agendamento.java
@@ -5,8 +5,8 @@ import java.time.LocalDateTime;
 public class Agendamento {
 
 	private long idAgendamento;
-	private LocalDateTime horarioInicio;
-	private LocalDateTime horarioFim;
+	private LocalDateTime dia;
+	private StausAtendimento statusAtendimento;
 	private String descricaoFinal;
 	private Aluno aluno;
 	private Monitor monitor;
@@ -14,87 +14,23 @@ public class Agendamento {
 
 	public Agendamento() {
 		super();
-		horarioInicio = LocalDateTime.now();
-		horarioFim = LocalDateTime.now();
+		dia = LocalDateTime.now();
+		statusAtendimento - new StausAtendimento();
 		descricaoFinal = "";
 		aluno = new Aluno();
 		monitor = new Monitor();
 		disciplina = new Disciplina();
 	}
 
-	public Agendamento(long idAgendamento, String status, LocalDateTime horarioInicio, LocalDateTime horarioFim,
+	public Agendamento(long idAgendamento, LocalDateTime dia, StausAtendimento statusAtendimento,
 			String descricaoFinal, Aluno aluno, Monitor monitor, Disciplina disciplina) {
 		super();
 		this.idAgendamento = idAgendamento;
-		this.horarioInicio = horarioInicio;
-		this.horarioFim = horarioFim;
+		this.dia = dia;
+		this.statusAtendimento = statusAtendimento;
 		this.descricaoFinal = descricaoFinal;
 		this.aluno = aluno;
 		this.monitor = monitor;
 		this.disciplina = disciplina;
 	}
-
-	public long getIdAgendamento() {
-		return idAgendamento;
-	}
-
-	public void setIdAgendamento(long idAgendamento) {
-		this.idAgendamento = idAgendamento;
-	}
-
-	public LocalDateTime getHorarioInicio() {
-		return horarioInicio;
-	}
-
-	public void setHorarioInicio(LocalDateTime horarioInicio) {
-		this.horarioInicio = horarioInicio;
-	}
-
-	public LocalDateTime getHorarioFim() {
-		return horarioFim;
-	}
-
-	public void setHorarioFim(LocalDateTime horarioFim) {
-		this.horarioFim = horarioFim;
-	}
-
-	public String getDescricaoFinal() {
-		return descricaoFinal;
-	}
-
-	public void setDescricaoFinal(String descricaoFinal) {
-		this.descricaoFinal = descricaoFinal;
-	}
-
-	public Aluno getAluno() {
-		return aluno;
-	}
-
-	public void setAluno(Aluno aluno) {
-		this.aluno = aluno;
-	}
-
-	public Monitor getMonitor() {
-		return monitor;
-	}
-
-	public void setMonitor(Monitor monitor) {
-		this.monitor = monitor;
-	}
-
-	public Disciplina getDisciplina() {
-		return disciplina;
-	}
-
-	public void setDisciplina(Disciplina disciplina) {
-		this.disciplina = disciplina;
-	}
-
-	@Override
-	public String toString() {
-		return "Agendamento [idAgendamento=" + idAgendamento + ", horarioInicio=" + horarioInicio + ", horarioFim="
-				+ horarioFim + ", descricaoFinal=" + descricaoFinal + ", aluno=" + aluno + ", monitor=" + monitor
-				+ ", disciplina=" + disciplina + "]";
-	}
-
 }
